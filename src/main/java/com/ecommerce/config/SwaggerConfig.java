70c5a2d27fb72456ef0d6e33b318c28997e94588
Bibin <bibinfrancis404@outlook.com>
2021-10-27 14:34:44 +0530

    config changes

--- src/main/java/com/ecommerce/config/SwaggerConfig.java
+++ src/main/java/com/ecommerce/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
 	@Bean
 	public Docket apiDoc() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-		.apis(RequestHandlerSelectors.basePackage("com.simplilearn.webservice.controller"))
+		.apis(RequestHandlerSelectors.basePackage("com.ecommerce.controller"))
 		.build().apiInfo(metaData());
 	}
 
